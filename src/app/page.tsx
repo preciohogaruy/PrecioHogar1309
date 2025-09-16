ecfee78e7b14fe1f96ca981b451065502a3b65bd
Precio Hogar <preciohogar638@gmail.com>
2025-09-16 12:15:56 +0000

    no quiero que el fondo sea blanco como se ve actualmente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import { DynamicMapLoader } from "@/components/DynamicMapLoader"
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-white overflow-x-hidden">
+    <div className="min-h-screen overflow-x-hidden">
       <Navbar />
       <main>
         <HeroSection />
