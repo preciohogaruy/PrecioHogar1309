00574bd9fe3139b836c0e2a05ea11df9fcb51930
Precio Hogar <preciohogar638@gmail.com>
2025-09-18 05:38:41 +0000

    Puedes crear un archivo llamado imgactivas que devuelva todas las imágen

---
+++ src/lib/imgactivas.ts
@@ -0,0 +1,14 @@
+
+export const activeImages = [
+    "/logotienda.svg",
+    "/logotienda_blanco.svg",
+    "/placeholder.svg",
+    "/assets/muebles.png",
+    "/banners/banner_1.jpg",
+    "/banners/banner_2.png",
+    "/banners/banner_3.jpg",
+    "/banners/banner_4.jpg",
+    "/banners/banner_registro.png",
+    "/fondos/fondo_productos.svg",
+    "/fondos/logo_horizontal_inicio_con_fondo.svg",
+];
